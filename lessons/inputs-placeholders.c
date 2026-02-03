05aafd8de1a86b3d417c991bf8f85ef1c96fbbce
Charmingdc <charmingdc002@gmail.com>
2026-02-03 11:22:07 +0100

    First commit

---
+++ lessons/inputs-placeholders.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("An integer: %d\n", 18);
+  printf("A large integer: %ld\n", 1838484849338);
+  printf("A float: %f\n", 18.0);
+  printf("A large float: %lf\n", 18.37383748383838);
+  printf("A character: %c\n", 'm');
+  printf("A string: %s\n,", "Learning about inputs placeholders in C");
+}
